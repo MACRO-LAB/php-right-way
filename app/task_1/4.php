e1ab5c2e60dc2cf0e2f368f4b0a6998b78c577dd
VIKTOR_TIMOFEEV <macrolabs@mail.ru>
2023-10-14 00:16:34 +0800

    Часть 1. Задания к урокам 11-20

--- app/task_1/4.php
+++ app/task_1/4.php
@@ -2,15 +2,15 @@
 echo '</br>' . '_____________4 задание' . '</br>';
 $x = 200;
 $path = $_SERVER['DOCUMENT_ROOT'];
-switch ($x)
-{
-	case '200':
-		require $path . "/index.php";
-		break;
-	case '404':
-		require $path . "/404.php";
-		break;
-}
+//switch ($x)
+//{
+//	case '200':
+//		require $path . "/index.php";
+//		break;
+//	case '404':
+//		require $path . "/404.php";
+//		break;
+//}
 
 match ("$x"){
 	'200'=>require $path . "/index.php",
--- app/task_1/5.php
+++ app/task_1/5.php
@@ -1,2 +1,47 @@
 <?php
-declare(strict_types=1);
+//declare(strict_types = 1);
+echo '</br>' . '_____________5 задание' . '</br>';
+
+$arr5 = [
+	'error-control' => '@',
+
+	'assignment' => '= += -= *= /= %= **=',
+	'string' => '. .=',
+	'comparison' => '== === != !== <> <= >= ?? ?:',
+	'logical' => 'or || && and !',
+	'in/de-crement' => '-- ++',
+	'bitwise' => '~ ^ & | << >>',
+	'array' => '+ == === != <> !==',
+	'execution' => '``',
+	'type' => 'instanceof',
+	'nullsafe' => '?',
+	'arithmetic' => '+ - / % **',
+];
+
+function search($x)
+{
+	$arr = [
+		'error-control' => '@',
+		'arithmetic' => '+ - / % **',
+		'assignment' => '= += -= *= /= %= **=',
+		'string' => '. .=',
+		'comparison' => '== === != !== <> <= >= ?? ?:',
+		'logical' => 'or || && and !',
+		'in/de-crement' => '-- ++',
+		'bitwise' => '~ ^ & | << >>',
+		'array' => '+ == === != <> !==',
+		'execution' => '``',
+		'type' => 'instanceof',
+		'nullsafe' => '?',
+
+	];
+	foreach ($arr as $key => $item)
+	{
+		$pp = explode(' ', $item);
+		if (in_array($x, $pp))
+		{
+			echo "Оператор '$x' относится к операторам $key. </br>";
+		}
+	}
+}
+search('+');
