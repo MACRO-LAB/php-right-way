38f98a4a92ae54b7d0edbfbbdc31c63719434a92
VIKTOR_TIMOFEEV <macrolabs@mail.ru>
2023-10-16 00:52:56 +0800

    Задания к урокам 21-26 birthday-end

--- app/task_1/6.php
+++ app/task_1/6.php
@@ -1,9 +1,8 @@
 <?php
 require_once '6_data.php';
 /**
- *  @var array $allData
+ * @var array $allData
  */
-
 
 /**
  * @return ID
@@ -13,58 +12,146 @@ function getId()
 	static $id = 0;
 	$id++;
 	return $id;
-};
+}
+
+;
 
 /**
  * @return переводит символы с кириллицы на латиницу
  */
-function toLat($text) {
+function toLat($text)
+{
 	$converter = array(
-		'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'e', 'ж' => 'zh',
-		'з' => 'z', 'и' => 'i', 'й' => 'y', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o',
-		'п' => 'p', 'р' => 'r', 'с' => '', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c',
-		'ч' => 'ch', 'ш' => 'h', 'щ' => 'ch', 'ъ' => '', 'ы' => 'y', 'ь' => '', 'э' => 'e', 'ю' => 'yu',
+		'а' => 'a',
+		'б' => 'b',
+		'в' => 'v',
+		'г' => 'g',
+		'д' => 'd',
+		'е' => 'e',
+		'ё' => 'e',
+		'ж' => 'zh',
+		'з' => 'z',
+		'и' => 'i',
+		'й' => 'y',
+		'к' => 'k',
+		'л' => 'l',
+		'м' => 'm',
+		'н' => 'n',
+		'о' => 'o',
+		'п' => 'p',
+		'р' => 'r',
+		'с' => '',
+		'т' => 't',
+		'у' => 'u',
+		'ф' => 'f',
+		'х' => 'h',
+		'ц' => 'c',
+		'ч' => 'ch',
+		'ш' => 'h',
+		'щ' => 'ch',
+		'ъ' => '',
+		'ы' => 'y',
+		'ь' => '',
+		'э' => 'e',
+		'ю' => 'yu',
 		'я' => 'ya',
-		'А' => 'A', 'Б' => 'B', 'В' => 'V', 'Г' => 'G', 'Д' => 'D', 'Е' => 'E', 'Ё' => 'E', 'Ж' => 'Zh',
-		'З' => 'Z', 'И' => 'I', 'Й' => 'Y', 'К' => 'K', 'Л' => 'L', 'М' => 'M', 'Н' => 'N', 'О' => 'O',
-		'П' => 'P', 'Р' => 'R', 'С' => 'S', 'Т' => 'T', 'У' => 'U', 'Ф' => 'F', 'Х' => 'H', 'Ц' => 'C',
-		'Ч' => 'Ch', 'Ш' => 'Sh', 'Щ' => 'Sch', 'Ъ' => '', 'Ы' => 'Y', 'Ь' => '', 'Э' => 'E', 'Ю' => 'Yu',
+		'А' => 'A',
+		'Б' => 'B',
+		'В' => 'V',
+		'Г' => 'G',
+		'Д' => 'D',
+		'Е' => 'E',
+		'Ё' => 'E',
+		'Ж' => 'Zh',
+		'З' => 'Z',
+		'И' => 'I',
+		'Й' => 'Y',
+		'К' => 'K',
+		'Л' => 'L',
+		'М' => 'M',
+		'Н' => 'N',
+		'О' => 'O',
+		'П' => 'P',
+		'Р' => 'R',
+		'С' => 'S',
+		'Т' => 'T',
+		'У' => 'U',
+		'Ф' => 'F',
+		'Х' => 'H',
+		'Ц' => 'C',
+		'Ч' => 'Ch',
+		'Ш' => 'Sh',
+		'Щ' => 'Sch',
+		'Ъ' => '',
+		'Ы' => 'Y',
+		'Ь' => '',
+		'Э' => 'E',
+		'Ю' => 'Yu',
 		'Я' => 'Ya',
 	);
 	return strtr($text, $converter);
 }
-function randGender(){
-	global $allData;
-	$randGender = array_rand($allData['gender']);
-	return $randGender;
-}
-function generateMail()
+
+function randGender()
 {
 	global $allData;
-	$randGender =randGender();
-	$countLastNAme = rand(0, (count($allData['lastName']) - 1));
-	$latName='';
-	$latLastName ='';
-	if ($randGender == 'male')
+	return array_rand($allData['gender']);
+}
+
+function randName($gend)
+{
+	global  $allData;
+	$name = '';
+	if ($gend == 'male')
 	{
 		$countGender = rand(0, (count($allData['gender']['male']) - 1));
-		$latName = toLat($allData['gender']['male'][$countGender]);
-		$latLastName = toLat($allData['lastName'][$countLastNAme]);
-	}elseif ($randGender == 'female'){
-		$countGender = rand(0, (count($allData['gender']['female']) - 1));
-		$latName = toLat($allData['gender']['female'][$countGender]);
-		$latLastName = toLat($allData['lastName'][$countLastNAme]) . 'a';
+		$name = $allData['gender']['male'][$countGender];
 	}
-	$countDom = rand(0, (count($allData['domains']) - 1));
-	$domain = $allData['domains'][$countDom];
-	return strtolower($latName . $latLastName . '@' . $domain);
-}
-function birthday($max=1104537600, $min=0){
-	$randTime = rand(0,1104537600);
-	return date('d.m.Y', $randTime);
+	else if ($gend == 'female')
+	{
+		$countGender = rand(0, (count($allData['gender']['female']) - 1));
+		$name = $allData['gender']['female'][$countGender];
+	}
+	return $name;
 
 }
-function createUsers($size) {
+
+function randLastName($gend)
+{
+	global $allData;
+	$lastName = '';
+	$countLastNAme = rand(0, (count($allData['lastName']) - 1));
+	if ($gend == 'male')
+	{
+		$lastName = $allData['lastName'][$countLastNAme];
+	}
+	else if ($gend == 'female')
+	{
+		$lastName = $allData['lastName'][$countLastNAme] . 'a';
+	}
+	return $lastName;
+}
+
+function generateMail($name,$lastName)
+{
+	global $allData;
+	$countDom = rand(0, (count($allData['domains']) - 1));
+	$nameLAT=toLat($name);
+	$nameLAstLAT=toLat($lastName);
+	$domain = $allData['domains'][$countDom];
+	return strtolower($nameLAT[0] . $nameLAstLAT . '@' . $domain);
+}
+
+function birthday($max, $min = 0)
+{
+	$strtotime = strtotime($max);
+	$randTime = rand(0, $strtotime);
+	return date('d.m.Y', $randTime);
+}
+
+function createUsers($size)
+{
+	global $allData;
 
 	$main = [
 		'id' => '',
@@ -78,17 +165,17 @@ function createUsers($size) {
 		'alary' => '',
 		'age' => '',
 	];
-	$main['id']=getId();
-	$main['gender']=randGender();
-	$main['name'] ='';
-	$main['last_name'] ='';
-	$main['birthday'] = birthday();
-	$main['mail'] = generateMail();
+	$main['id'] = getId();
+	$randGender = randGender();
+	$main['gender'] = $randGender;
+	$main['name']= randName($randGender);
+	$main['last_name'] = randLastName($randGender);
+	$main['birthday'] = birthday('1.1.2000');
+	$main['mail'] = generateMail($main['name'],$main['last_name']);
 	echo "<pre>";
-	$mainEx=array_slice($main,0,$size);
+	$mainEx = array_slice($main, 0, $size);
 	print_r($mainEx);
 	echo "</br>";
-
 }
 createUsers(14);
 
